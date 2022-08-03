6c7cccf36f232ebffd49eeac543d951b9bd62bdd
Sam5196 <sammedpachore1008@gmail.com>
2022-08-03 15:14:20 +0530

    Added:Using Lambda function to validate first name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -16,5 +16,10 @@ public class UserRegistration {
         };
         System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Sammed"));
 
+        Validation isValidLastName = (pattern, lastName) -> {
+            return  "Last Name is " + Pattern.compile(pattern).matcher(lastName).matches();
+        };
+        System.out.println(isValidLastName.validate("^[A-Z]{1}[a-z]{2,}$", "Pachore"));
+
     }
 }
